d54f688082e9eca725176e5927c14200ed32036f
Namo <bentlypamela@gmail.com>
2019-02-06 12:15:18 -0800

    Working in class still, but now has comments

--- ClassPractice/ArrayIntList.java
+++ ClassPractice/ArrayIntList.java
@@ -1,12 +1,20 @@
 public class ArrayIntList{
+    /*
+    *This is the code that Coach Booth had us copy as he wrote
+    *It is supposed to create a random array, and then let us add ints to that array
+    *We can find indexs and all that jazz
+    *Also we can add and set those ints.. whoo!
+    */
     private int[]list;
     private int size;
 
+    //You do not always need this. but we used it because we just learned what it is
     public ArrayIntList(){
         this.list = new int[10];
         this.size = 0;
     }
 
+    //You don't want to cause an error by not having enough space
     private void checkSize(){
         if(this.size > this.list.length/4){
             int[] newList = new int[size*2];
